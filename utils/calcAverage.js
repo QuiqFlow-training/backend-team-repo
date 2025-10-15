530792ba147090515fc909cce90af115bbef3c3c
Ameer Majed Jamal Al-Khoury <khouryameer9@gmail.com>
2025-10-15 12:20:42 +0300

    Add Calculate Average util -- Ameer

---
+++ utils/calcAverage.js
@@ -0,0 +1,24 @@
+function calculateAverage(numbers) {
+
+    if(!Array.isArray(numbers)) {
+            throw new TypeError("calculateAverage expects an array of numbers");
+        }
+        
+    if (numbers.length === 0) return 0;
+
+    let sum = 0;
+
+    for (let i = 0 ; i < numbers.length; i++ ){
+
+        const n = Number(numbers[i]);
+
+        if(Number.isNaN(n)) {
+         throw new TypeError("All items in the array must be numbers or numeric strings");
+        }
+
+        sum += n;
+    }
+    return sum/numbers.length;
+
+}
+module.exports = {calculateAverage}
