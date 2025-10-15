7e25424fa9879da65a23c18be8b9b150df6f27ef
Ameer Majed Jamal Al-Khoury <khouryameer9@gmail.com>
2025-10-15 12:24:46 +0300

    Delete unnecessary file

--- utils/test-calcAverage.js
+++ utils/test-calcAverage.js
@@ -1,4 +1,4 @@
-let calc = require('../utils/calcAverage');
+let calc = require('./calcAverage');
 
 console.log('Average [10, 20, 30] =', calc.calculateAverage([10, 20, 30]));
 console.log('Average ["5", 15, 10] =', calc.calculateAverage(["5", 15, 10]));
