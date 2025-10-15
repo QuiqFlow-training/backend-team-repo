49daa9eb244872e6dc82e5660cd208288f851e6e
Majed Alqudah <141547667+MajedAlqudah@users.noreply.github.com>
2025-10-15 14:29:09 +0300

    Update User.js

--- src/User.js
+++ src/User.js
@@ -9,7 +9,18 @@ export default class User {
     this.profile = { bio: "", website: "" };
     User.users.push(this);
   }
+ login() {
+    this.isLoggedIn = true;
+    console.log(`${this.name} is logged in`);
+    return this;
+  }
 
+  updateProfile(newDetails) {
+    Object.assign(this, newDetails);
+    console.log(`${this.name}'s profile updated`);
+    return this;
+  }
+  
 logOut() {
     this.isLoggedIn = false;
   }
